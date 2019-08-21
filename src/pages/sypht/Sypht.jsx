e10db82a41cec496a435830f7d09e60ef17126d0
Andrew Adhigunarsa <adhigunarsa@192-168-1-106.tpgi.com.au>
2019-08-22 07:39:03 +1000

    deployment

--- src/pages/sypht/Sypht.jsx
+++ src/pages/sypht/Sypht.jsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
+const StyledH1 = styled.h1`
+    background-color: red;
+`;
 class Sypht extends React.Component{
     render(){
         return (
             <div>
-                <h1>Sypht</h1>
+                <StyledH1>Sypht</StyledH1>
                 <p>
                     This is the Main page
                 </p>
