3e7631fc801f0befe402c2751b687aef1e129c47
Andrew Adhigunarsa <adhigunarsa@192-168-1-106.tpgi.com.au>
2019-08-28 08:22:52 +1000

    first commit

--- src/pages/date-calculator/DateCalculator.jsx
+++ src/pages/date-calculator/DateCalculator.jsx
@@ -4,9 +4,19 @@ import styled from 'styled-components';
 const StyledDiv = styled.div `
     padding:30px;
 `;
+const StyledH1 = styled.h1`
+  margin-top: 0;
+`;
+
 class DateCalculator extends React.Component{
     render(){
-        return (<StyledDiv>DateCalculator</StyledDiv>);
+        return (
+          <StyledDiv>
+           
+              <StyledH1>DateCalculator</StyledH1>
+            
+          </StyledDiv>
+        );
     }
 }
 
--- src/pages/pie-loader/PieLoader.jsx
+++ src/pages/pie-loader/PieLoader.jsx
@@ -4,9 +4,17 @@ import styled from 'styled-components';
 const StyledDiv = styled.div `
     padding:30px;
 `;
+
+const StyledH1 = styled.h1`
+  margin-top: 0;
+`;
 class PieLoader extends React.Component{
     render(){
-        return (<StyledDiv>PieLoader</StyledDiv>);
+        return (
+          <StyledDiv>
+            <StyledH1>Pie Loader</StyledH1>
+          </StyledDiv>
+        );
     }
 }
 
